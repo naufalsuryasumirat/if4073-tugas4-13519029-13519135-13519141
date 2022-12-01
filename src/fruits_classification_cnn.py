8deae2dfde90d3a2e8212da232c1130d38483c1a
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2022-12-01 23:53:23 +0700

    feat: model xception, driver update

--- src/fruits_classification_cnn.py
+++ src/fruits_classification_cnn.py
@@ -1,27 +1,47 @@
+import os
 import keras
 import pickle
 import cv2 as cv
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from pathlib import Path
 from typing import List, Tuple
+from keras.api._v2.keras.preprocessing.image import ImageDataGenerator
 
 path = Path(__file__).resolve().parent
 
-model: keras.Model = keras.models.load_model(f'{path/"models/baseline_cnn_model_4"}', compile=False)
+models_path = [
+  'baseline_cnn_model_xception_5',
+  'baseline_cnn_model_4',
+]
+
+model_name = None
+for p in models_path:
+  if os.path.exists(f'{path/f"models/{p}"}'): model_name = p; break
+
+model: keras.Model = keras.models.load_model(f'{path/f"models/{model_name}"}', compile=False)
+
+datagen = ImageDataGenerator(rescale=1./255)\
+  if 'xception' in model_name else\
+    ImageDataGenerator(rescale=1./255,
+                       preprocessing_function=tf.keras.applications.vgg16.preprocess_input)
 
 pred_labels = {}
-with open(f'{path/"dict_softmax/baseline_cnn_model_4.pkl"}', 'rb') as fl:
+with open(f'{path/f"dict_softmax/{model_name}.pkl"}', 'rb') as fl:
   pred_labels = pickle.load(fl)
 
 def predict_fruit(path: str) -> Tuple[str, List[Tuple[str, float]]]:
-  image = cv.imread(path)
-  image = cv.resize(image, dsize=(224, 224))
-  image = image.astype('float64') / 255
-  image = image.reshape(1, 224, 224, 3)
-  image = tf.convert_to_tensor(image, dtype='float64')
-  image = tf.keras.applications.vgg16.preprocess_input(image)
+  image = datagen.flow_from_dataframe(
+    dataframe=pd.DataFrame(columns=['Path', 'Label'], data=[[path, '']]),
+    x_col='Path',
+    y_col='Label',
+    target_size=(224,224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=1,
+    shuffle=False)
 
   pred: tf.Tensor = model.predict(image)
   sureness = [(pred_labels[i], conf) for (i, conf) in enumerate(pred[0])]
@@ -30,4 +50,6 @@ def predict_fruit(path: str) -> Tuple[str, List[Tuple[str, float]]]:
   return pred_labels[pred[0]], sureness
 
 if __name__ == '__main__':
-  print(predict_fruit('data/test/banana/331.jpg'))
+  print(pred_labels)
+  print(predict_fruit('data/test/durian/950.jpg'))
+  print(predict_fruit('data/test/lime/197.jpg'))
