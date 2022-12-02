d51a5a9b79ba5739b7304b9aa617399c34187c34
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2022-12-02 20:14:57 +0700

    feat: update gui, driver im proc, model svm

--- app.py
+++ app.py
@@ -1,69 +1,102 @@
+import io
 import sys
 import numpy as np
 import PyQt5.QtCore as qtc
 import PyQt5.QtWidgets as qtw
+import matplotlib.pyplot as plt
 import src.fruits_classification_cnn as cls_cnn_model
 import src.fruits_classification_improc as improc
 from PIL import Image, ImageQt
 from matplotlib.cm import get_cmap
 from PyQt5.QtWidgets import (
-  QApplication, QPushButton, QMainWindow, QLabel, QTextEdit, QFileDialog)
+  QApplication, QPushButton, QMainWindow, QLabel, QFileDialog)
 from PyQt5.QtGui import QFont, QPixmap
+from cycler import cycler
+
+plt.rcParams['figure.figsize'] = [4, 2.5]
+plt.rcParams['figure.autolayout'] = True
+plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b'])))
 
 class Window(QMainWindow):
   def __init__(self):
     super().__init__()
     self.setStyleSheet('background-color: #F4F2E9')
     self.setWindowTitle('Klasifikasi Buah')
-    self.setGeometry(0, 0, 1400, 800)
-    self.setMinimumSize(1000, 800)
-    # self.setFixedSize(1400, 800)
-
+    self.setGeometry(0, 0, 1500, 800)
+    self.setMinimumSize(1200, 800)
+    
     # Labels
     self._lbl_title = QLabel('IF4073-Tugas4-Klasifikasi Buah', self)
     self._lbl_title.move(10, 10)
     self._lbl_title.resize(700, 35)
     self._lbl_title.setFont(QFont('AnyStyle', 20, 500))
     self._lbl_title.setStyleSheet('color: #372213;')
-    
+
     self._lbl_input = QLabel('Input', self)
     self._lbl_input.move(10, 40)
     self._lbl_input.setFont(QFont('AnyStyle', 14, 30))
     self._lbl_input.setStyleSheet('color: #372213;')
+
+    self._lbl_segmt = QLabel('Segmented', self)
+    self._lbl_segmt.move(276, 40)
+    self._lbl_segmt.setFont(QFont('AnyStyle', 14, 30))
+    self._lbl_segmt.setStyleSheet('color: #372213;')
     
-    self._lbl_dl_info = QLabel('Deep Learning Model Prediction (VGG 16 Transfer Learning w/ ImageNet weights)', self)
+    self._lbl_grays = QLabel('Grayscale', self)
+    self._lbl_grays.move(542, 40)
+    self._lbl_grays.setFont(QFont('AnyStyle', 14, 30))
+    self._lbl_grays.setStyleSheet('color: #372213;')
+    
+    self._lbl_contr = QLabel('Contour', self)
+    self._lbl_contr.move(808, 40)
+    self._lbl_contr.setFont(QFont('AnyStyle', 14, 30))
+    self._lbl_contr.setStyleSheet('color: #372213;')
+    
+    self._lbl_mlinf = QLabel('SVM Model (w/ features shown)', self)
+    self._lbl_mlinf.move(1074, 400)
+    self._lbl_mlinf.resize(400, 40)
+    self._lbl_mlinf.setFont(QFont('AnyStyle', 14, 100))
+    self._lbl_mlinf.setStyleSheet('color: #372213;')
+    
+    self._lbl_mpred = QLabel('', self)
+    self._lbl_mpred.move(1074, 450)
+    self._lbl_mpred.resize(400, 40)
+    self._lbl_mpred.setFont(QFont('Courier', 14, 5))
+    self._lbl_mpred.setStyleSheet('color: ##372213;')
+    
+    self._lbl_dl_info = QLabel('Deep Learning Model Prediction (VGG 16 || XCeption Transfer Learning w/ ImageNet weights)', self)
     self._lbl_dl_info.move(10, 600)
-    self._lbl_dl_info.resize(800, 35)
+    self._lbl_dl_info.resize(1000, 35)
     self._lbl_dl_info.setFont(QFont('AnyStyle', 14, 100))
     self._lbl_dl_info.setStyleSheet('color: #372213;')
-    
+
     self._lbl_dl_predict = QLabel('Predicted Label: ', self)
     self._lbl_dl_predict.move(10, 650)
     self._lbl_dl_predict.resize(150, 35)
     self._lbl_dl_predict.setFont(QFont('Roboto', 14, 10))
     self._lbl_dl_predict.setStyleSheet('color: #372213;')
-    
-    self._lbl_dl_predict_res = QLabel('test_label', self)
+
+    self._lbl_dl_predict_res = QLabel('', self)
     self._lbl_dl_predict_res.move(180, 650)
     self._lbl_dl_predict_res.resize(400, 35)
     self._lbl_dl_predict_res.setFont(QFont('Courier', 12, 5))
     self._lbl_dl_predict_res.setStyleSheet('color: #372213;')
     self._lbl_dl_predict_res.setWordWrap(True)
-    
+
     self._lbl_dl_confidence = QLabel('Confidence: ', self)
     self._lbl_dl_confidence.move(10, 675)
     self._lbl_dl_confidence.resize(100, 100)
     self._lbl_dl_confidence.setFont(QFont('Roboto', 14, 10))
     self._lbl_dl_confidence.setStyleSheet('color: #372213;')
     self._lbl_dl_confidence.setWordWrap(True)
-    
-    self._lbl_dl_confidence_res = QLabel('test_conf', self)
+
+    self._lbl_dl_confidence_res = QLabel('', self)
     self._lbl_dl_confidence_res.move(120, 675)
     self._lbl_dl_confidence_res.resize(700, 100)
     self._lbl_dl_confidence_res.setFont(QFont('Courier', 12, 5))
     self._lbl_dl_confidence_res.setStyleSheet('color: #372213;')
     self._lbl_dl_confidence_res.setWordWrap(True)
-    
+
     # Buttons
     self._btn_load = QPushButton(self)
     self._btn_load.move(465, 13)
@@ -84,20 +117,23 @@ class Window(QMainWindow):
     self._btn_process.setFixedWidth(150)
     self._btn_process.setStyleSheet('QPushButton { background-color: #372213; color: #F4F2E9; }'
                                'QPushButton::pressed { background-color: #F4F2E9; color: #372213; }')
-    
+
     # Images
     self._img_input = QLabel(self)
     self._img_input.move(10, 70)
-    
+
     self._img_segmt = QLabel(self)
     self._img_segmt.move(276, 70)
-    
+
     self._img_grays = QLabel(self)
     self._img_grays.move(542, 70)
-    
+
     self._img_imhog = QLabel(self)
     self._img_imhog.move(808, 70)
     
+    self._img_plots = QLabel(self)
+    self._img_plots.move(1074, 70)
+
     self._cur_filepath = None
 
     self.show()
@@ -118,25 +154,43 @@ class Window(QMainWindow):
     self._lbl_dl_predict_res.setText(f"{label}")
     self._lbl_dl_confidence_res.setText(
       f"{'; '.join([f'{lbl}={conf*100:.2f}%' for lbl, conf in confidence])}")
-    
+
     img = improc.load_image(self._cur_filepath)
     segmented = improc.proc_segment(img)
     segmented_show = ImageQt.ImageQt(Image.fromarray(segmented, mode='RGB'))
     self._img_segmt.resize(64*4, 128*4)
     self._img_segmt.setPixmap(QPixmap.fromImage(segmented_show))
-    
+
     grayscale = improc.gray_image(segmented)
     grayscale_show = ImageQt.ImageQt(Image.fromarray(np.uint8(grayscale*255), mode='L'))
     self._img_grays.resize(64*4, 128*4)
     self._img_grays.setPixmap(QPixmap.fromImage(grayscale_show))
-    
+
     cm_viridis = get_cmap('viridis')
-    fd, hog_im = improc.proc_hog(grayscale)
+    ft_hog, hog_im = improc.proc_hog(grayscale)
     hog_im = cm_viridis(hog_im)
     hog_show = ImageQt.ImageQt(Image.fromarray(np.uint8(hog_im*255)))
     self._img_imhog.resize(64*4, 128*4)
     self._img_imhog.setPixmap(QPixmap.fromImage(hog_show))
-    
+
+    ft_color, (red, grn, blu) = improc.proc_color(segmented)
+    ranges = range(256)
+    self._img_buf = io.BytesIO()
+    plt.plot(ranges, red, ranges, grn, ranges, blu)
+    plt.title('Color Histogram of Segmented Image')
+    plt.legend(['red', 'green', 'blue'], loc='upper left')
+    plt.savefig(self._img_buf, format='png')
+    plot_img = Image.open(self._img_buf)
+    plot_show = ImageQt.ImageQt(plot_img)
+    self._img_plots.resize(plot_img.width, plot_img.height)
+    self._img_plots.setPixmap(QPixmap.fromImage(plot_show))
+
+    ft_glcm = improc.proc_glcm(grayscale)
+
+    features = np.concatenate([ft_hog, ft_color, ft_glcm])
+    ml_predicted = improc.predict_fruit(features.reshape(1, -1))
+    self._lbl_mpred.setText(str(ml_predicted[0]))
+
     return
 
 if __name__ == '__main__':
--- src/fruits_classification_improc.py
+++ src/fruits_classification_improc.py
@@ -1,22 +1,34 @@
+import bz2
+import pickle
 import cv2 as cv
 import numpy as np
 import pandas as pd
+from pathlib import Path
+from sklearn.svm import SVC
 from skimage import color
 from skimage.io import imread
-from skimage.feature import hog
+from skimage.feature import hog, graycomatrix, graycoprops
 from skimage.transform import resize
 from typing import List, Union, Tuple
+
+path = Path(__file__).resolve().parent
+
+IMAGE = List[List[List[Union[int, float]]]]
+
+file = bz2.BZ2File(f'{path/"models/ml_svm_model_compressed.pbz2"}')
+ml_model: SVC = pickle.load(file)
+file.close()
 
 def imshape(mat: List[List[Union[List[int], int]]]) -> Tuple[int, int, int]:
   shapes = np.shape(mat)
   dim = len(shapes)
   return *shapes[:2], shapes[2] if dim > 2 else None
 
-def load_image(path: str) -> List[List[List[float]]]:
+def load_image(path: str) -> IMAGE:
   img = imread(path)
   return resize(img, (128*4, 64*4))
 
-def proc_segment(image) -> None:
+def proc_segment(image) -> IMAGE:
   img = (np.array(image) * 255).round().astype(np.uint8)
   edge = cv.Canny(img, threshold1=0, threshold2=255)
   edge = cv.dilate(edge, None)
@@ -26,15 +38,80 @@ def proc_segment(image) -> None:
   segmented = cv.bitwise_and(img, img, mask=mask)
   return segmented
 
-def gray_image(image: List[List[List[int]]]) -> List[List[List[int]]]:
+def gray_image(image: IMAGE) -> IMAGE:
   return color.rgb2gray(np.array(image)[:,:,:3])
 
-def proc_hog(image) -> None:
+def proc_hog(image: IMAGE) -> Tuple[List[float], IMAGE]:
   fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16,16),
                       cells_per_block=(2,2), visualize=True,
                       feature_vector=True, block_norm='L2')
   return fd, hog_image
 
+def proc_color(image: IMAGE) -> Tuple[List[float], Tuple[List[int], List[int], List[int]]]:
+  """algorithm to count color values of rgb without counting black colors (segmented)"""
+  row, col, dim = imshape(image)
+  func = lambda x: x[0] * col + x[1]
+  indices = np.where((image[:,:,0] | image[:,:,1] | image[:,:,2]) == 0) # get indices of black pixels
+  # using bitwise or of three image channels
+  if type(indices) == tuple: indices = np.array(indices).T
+  indices = np.apply_along_axis(func, 1, indices)
+
+  red, grn, blu = [np.delete(image[:,:,d], indices) for d in range(dim)]
+  hists = [[0 for _ in range(256)] for _ in range(dim)]
+
+  for i, arr in enumerate([red, grn, blu]):
+    val, count = np.unique(arr, return_counts=True)
+    for j, v in enumerate(val): hists[i][v] = count[j]
+
+  return np.concatenate(hists) / (row * col), hists
+
+def proc_color_2(image: IMAGE) -> Tuple[List[float], Tuple[List[int], List[int], List[int]]]:
+  """algorithm 2 to count color values of rgb without counting black colors (segmented)"""
+  def getFrequency(img_channel):
+    freq = [0 for _ in range(256)]
+    for arr in img_channel:
+      freq[arr] += 1
+    return freq
+
+  temp = []
+  for i in range(len(image)):
+    for j in range(len(image[0])):
+      if image[i][j][0] != 0 or image[i][j][1] != 0 or image[i][j][2] != 0: temp.append(image[i][j])
+
+  temp2 = np.array(temp)
+  red = getFrequency(temp2[:,0])
+  green = getFrequency(temp2[:,1])
+  blue = getFrequency(temp2[:,2])
+
+  return np.concatenate([red, green, blue]) / (len(image) * len(image[0])), (red, green, blue)
+
+def proc_glcm(image: IMAGE) -> IMAGE:
+  glcm = graycomatrix((image * 255).round().astype(np.uint8), 
+                      distances=[5], 
+                      angles=[0, np.pi/4, np.pi/2, 3*np.pi/4], 
+                      levels=256,
+                      symmetric=True, 
+                      normed=True)
+  props = ['dissimilarity', 'correlation', 'homogeneity', 'contrast', 'ASM']
+  feature = [prop for name in props for prop in graycoprops(glcm, name)[0]]
+
+  return np.array(feature) / 1000 # normalize
+
+def predict_fruit(features: List[float]) -> str:
+  return ml_model.predict(features)
 
 if __name__ == '__main__':
-  print()
+  import time
+
+  img = load_image('data/test/apple/Image_2.jpg')
+  img = proc_segment(img)
+
+  start = time.time()
+  colors_1, _ = proc_color(img)
+  print(time.time() - start)
+
+  start = time.time()
+  colors_2, _ = proc_color_2(img)
+  print(time.time() - start)
+
+  print(all(colors_1 == colors_2))
